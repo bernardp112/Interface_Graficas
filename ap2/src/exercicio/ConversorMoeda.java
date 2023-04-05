198053137fa1ad9bcfc387a7521a9b9164fbab9e
Bernardo de Souza Silva <127486907+bernardp112@users.noreply.github.com>
2023-04-05 12:00:13 -0300

    Update ConversorMoeda.java

--- ap2/src/exercicio/ConversorMoeda.java
+++ ap2/src/exercicio/ConversorMoeda.java
@@ -1,76 +1,100 @@
 package exercicio;
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class ConversorMoeda extends JFrame implements ActionListener {
-    private JLabel valorLabel, resultadoLabel;
-    private JTextField valorTextField;
-    private JCheckBox dolarCheckBox, euroCheckBox, libraCheckBox;
-    private JButton converterButton;
+
+	private JLabel lblValor, lblResultado;
+    private JTextField txtFieldValor;
+    private JCheckBox cbDolar, cbEuro, cbLibra;
+    private JButton btnConverter;
 
     public ConversorMoeda() {
-        super("Conversor de Moeda");
+        
+        JFrame janela = new JFrame("Conversor de Moeda");
+		JPanel painel = new JPanel();
+       
+		janela.add(painel);
+		painel.setLayout(new GridLayout(4, 2));
 
-        Container container = getContentPane();
-        container.setLayout(new GridLayout(4, 2));
+        lblValor = new JLabel("Digite o valor em Real:");
+        lblResultado = new JLabel("Resultado");
+        
+        txtFieldValor = new JTextField();
+       
+        cbDolar = new JCheckBox("Dólar");
+        cbEuro = new JCheckBox("Euro");
+        cbLibra = new JCheckBox("Libra Esterlina");
+        
+        btnConverter = new JButton("Converter");
+        btnConverter.addActionListener(this);
+        
+        painel.add(lblValor);
+        painel.add(txtFieldValor);
+        painel.add(cbDolar);
+        painel.add(cbEuro);
+        painel.add(cbLibra);
+        painel.add(btnConverter);
+        painel.add(lblResultado);
 
-        valorLabel = new JLabel("Digite o valor em Real:");
-        container.add(valorLabel);
-
-        valorTextField = new JTextField();
-        container.add(valorTextField);
-
-        dolarCheckBox = new JCheckBox("Dólar");
-        container.add(dolarCheckBox);
-
-        euroCheckBox = new JCheckBox("Euro");
-        container.add(euroCheckBox);
-
-        libraCheckBox = new JCheckBox("Libra Esterlina");
-        container.add(libraCheckBox);
-
-        converterButton = new JButton("Converter");
-        converterButton.addActionListener(this);
-        container.add(converterButton);
-
-        resultadoLabel = new JLabel("Resultado");
-        container.add(resultadoLabel);
-
-        setSize(400, 150);
-        setVisible(true);
+        janela.setLocation(500, 300);
+		janela.pack();
+		janela.setVisible(true);
         
     }
 
     public void actionPerformed(ActionEvent event) {
-        double valorReal = Double.parseDouble(valorTextField.getText());
+        
+    	double valorReal = Double.parseDouble(txtFieldValor.getText());
 
-        if (dolarCheckBox.isSelected()) {
-            double valorDolar = valorReal * 0.18;
-            resultadoLabel.setText(String.format("Valor em Dólar: %.2f", valorDolar));
-        }
+    	if (cbDolar.isSelected() && cbLibra.isSelected() == false && cbEuro.isSelected() == false) {
+            
+    		double valorDolar = valorReal * 0.18;
+            lblResultado.setText(String.format("Valor em Dólar: %.2f", valorDolar));
+        
+    	}
 
-        if (euroCheckBox.isSelected()) {
+        else if (cbEuro.isSelected() && cbLibra.isSelected() == false && cbDolar.isSelected() == false) {
             
         	double valorEuro = valorReal * 0.15; 
-            resultadoLabel.setText(String.format("Valor em Euro: %.2f", valorEuro));
+        	lblResultado.setText(String.format("Valor em Euro: %.2f", valorEuro));
         
         }
 
-        if (libraCheckBox.isSelected()) {
+        else if (cbLibra.isSelected() && cbDolar.isSelected() == false && cbEuro.isSelected() == false) {
             
         	double valorLibra = valorReal * 0.13; 
-            resultadoLabel.setText(String.format("Valor em Libra Esterlina: %.2f", valorLibra));
+        	lblResultado.setText(String.format("Valor em Libra Esterlina: %.2f", valorLibra));
         
         }
+    	
+        else if (cbDolar.isSelected() == false && cbLibra.isSelected() == false && cbEuro.isSelected() == false) {
+        	
+        	lblResultado.setText(String.format("Por favor selecione uma Check Box!!"));
+        	
+        }
+    	
+        else {
+        	
+        	lblResultado.setText(String.format("Por favor selecione apenas uma Check Box!!"));
+        	
+        }
+        
     }
 
     public static void main(String args[]) {
         
-    	ConversorMoeda aplicacao = new ConversorMoeda();
-        aplicacao.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    
+    	ConversorMoeda janela = new ConversorMoeda();
+    	janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	
     }
     
 }
